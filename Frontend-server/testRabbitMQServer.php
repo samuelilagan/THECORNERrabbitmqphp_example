82ca87d5bc26aef28ed0d9a51b9219c71306ea55
dfc27 <dfc27@njit.edu>
2025-03-09 00:55:08 +0000

    added filtering as a case

--- Frontend-server/testRabbitMQServer.php
+++ Frontend-server/testRabbitMQServer.php
@@ -22,6 +22,8 @@ function requestProcessor($request)
             return doLogout($request['sessionToken']);
         case "register":
             return doRegister($request['username'], $request['password']);
+        case "filter":
+            return doFilter($request['city'], $request['keyword'], $request['rating']);
         default:
             return ["status" => "error", "message" => "Invalid request type"];
     }
