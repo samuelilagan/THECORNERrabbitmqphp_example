96f09337ed0dd27604fbcff6fe7c54d5c2fbfb3c
Melissa <mj479@njit.edu>
2025-04-14 10:51:32 -0400

    updated loglistener.php and tested successfully

--- Frontend-server/logger.php
+++ Frontend-server/logger.php
@@ -16,7 +16,7 @@ function log_error($message, $file = null, $line = null) {
         'timestamp' => $timestamp
     ]);
 // replace with ace's IP?
-    $connection = new AMQPStreamConnection('REPLACE_WITH_RABBITMQ_IP', 5672, 'guest', 'guest');
+    $connection = new AMQPStreamConnection('172.28.197.192', 5672, 'guest', 'guest');
     $channel = $connection->channel();
     $channel->queue_declare('logQueue', false, true, false, false);
 
