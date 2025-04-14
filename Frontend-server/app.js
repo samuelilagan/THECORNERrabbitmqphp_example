ad6632244cd13591938b36a2d150352a75657b0d
dfc27 <dfc27@njit.edu>
2025-04-14 18:16:32 +0000

    adding css to html files and adding css files

--- Frontend-server/app.js
+++ Frontend-server/app.js
@@ -2,78 +2,111 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 3001;
+
 
 //connects to notificationAPI
 const notificationapi = require('notificationapi-node-server-sdk').default
 notificationapi.init(
-    'nysaalf6rdm6jyg8pla5si4cju', // clientId
-    'xvsjcir9jktvmud6vtlpxnon8j217eh3uufighc0gcjlaw8uh9rdw815i9'// clientSecret
-  )
+   'nysaalf6rdm6jyg8pla5si4cju', // clientId
+   'xvsjcir9jktvmud6vtlpxnon8j217eh3uufighc0gcjlaw8uh9rdw815i9'// clientSecret
+ )
+
 
 // enables the ability to load otificaitonAPI asyncronously
 function loadScript(scriptUrl) {
-    let script = document.createElement("script");
-    script.src = scriptUrl;
-    script.type = "text/javascript";
-    script.async = true;  // Load asynchronously
-    document.body.appendChild(script);
+   let script = document.createElement("script");
+   script.src = scriptUrl;
+   script.type = "text/javascript";
+   script.async = true;  // Load asynchronously
+   document.body.appendChild(script);
 }
-//serve static files from 'it490' folder
-//app.use(express.static(path.join(__dirname, 'it490')));
+app.get('/notiflanding', (req, res) => {
+ res.sendFile(path.join(__dirname, 'notiflanding.html'));
+});
 
-//route to serve notiflanding.html
-app.get("/notiflanding.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "/", "notiflanding.html"));
-})
 
- 
+
 // Middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json()); // <--this line SHOULD parse JSON input from frontend
+
 
 // Serve the HTML form
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/notiftest.html');
+   res.sendFile(__dirname + '/notiftest.html');
 });
+
+
+// Serve notiflanding.html explicitly
+app.get('/notiflanding.html', (req, res) => {
+ res.sendFile(path.join(__dirname, 'notiflanding.html'));
+});
+
+
+//trying to allow port 3001 to access the stylesheets + slicknav
+app.get('/slicknav.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'slicknav.css'));
+ });
+
+app.get('/corner.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'corner.css'));
+ });
+ 
+app.get('https://code.jquery.com/jquery-3.6.0.min.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'https://code.jquery.com/jquery-3.6.0.min.js'));
+ });
+ app.get('/jquery.slicknav.min.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'jquery.slicknav.min.js'));
+ });
+
+
 
 // Handle form submission
-app.post('/submit', (req, res) => {
-    // Get data from the form
-    const email = req.body.email;
-    const number = req.body.number;
-
-    // Log the form data in the terminal
-    console.log('User Data:');
-    console.log('Number:', number);
-    console.log('Email:', email);
-
-    notificationapi.send({
-    notificationId: 'test',
-    user: {
-    id: "'" + email +"'",
-    email: "'" + email +"'",
-    number: "'+1'" + number +'"' // Replace with your phone number, use format [+][country code][area code][local number]
-  },
-  mergeTags: {
-    "comment": "You will have an upcoming reservation!",
-    "commentId": "testCommentId"
-  }
-})
+app.post('/submit', async (req, res) => {
+ try {
+   const email = req.body.email;
+   const number = req.body.number;
 
 
-    return email,number;
-    module.exports = email;
-    module.exports = number;
+   console.log('User Data Received:');
+   console.log('Email:', email);
+   console.log('Number:', number);
 
-    // Respond back to the user
-    res.send(`Thank you for your email, ${email}. We have received your information.`);
+
+   await notificationapi.send({
+     notificationId: 'test',
+     user: {
+     id: "'" + email +"'",
+     email: "'" + email +"'",
+     number: "'+1'" + number +'"' // Replace with your phone number, use format [+][country code][area code][local number]
+     },
+     mergeTags: {
+       "comment": "You will have an upcoming reservation!",
+       "commentId": "testCommentId"
+     }
+   });
+
+
+   res.status(200).json({ status: "success", message: `Notification sent to ${email}` });
+
+
+ } catch (err) {
+   console.error("Error in /submit:", err);
+   res.status(500).json({ status: "error", message: "Internal Server Error" });
+ }
 });
 
- 
+
+
+
+
+
+
 
 
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+   console.log(`Server is running on http://localhost:${port}`);
 });
